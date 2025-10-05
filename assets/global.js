1547bdfb17dc208d2ab385025a17fc1ecdf420a7
jubaer <jubaer.diu@gamil.com>
2025-10-05 07:26:01 +0600

    fix the animation

--- assets/global.js
+++ assets/global.js
@@ -284,7 +284,6 @@ function removeTrapFocus(elementToFocus = null) {
   document.removeEventListener("focusin", trapFocusHandlers.focusin);
   document.removeEventListener("focusout", trapFocusHandlers.focusout);
   document.removeEventListener("keydown", trapFocusHandlers.keydown);
-
   if (elementToFocus) elementToFocus.focus();
 }
 
